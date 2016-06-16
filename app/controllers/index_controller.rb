21d1d65f2501599df2d93bd7bd094bfd3681567e
Lawson Marlowe <lawsonmarlowe@gmail.com>
2016-06-16 16:43:19 -0400

    Add links from main page and css effects and shells for other direct pages

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,4 +4,32 @@ class IndexController < ApplicationController
       
 	end
 
+	def blog
+
+	end
+
+	def about
+
+	end
+
+	def contact
+
+	end
+
+	def projects
+
+	end
+
+	def resume
+
+	end
+
+	def about_vid
+
+	end
+
+	def legal
+
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'index#home'
+  get 'blog' => 'index#blog'
+  get 'about' => 'index#about'
+  get 'projects' => 'index#projects'
+  get 'contact' => 'index#contact'
+  get 'resume' => 'index#resume'
+  get 'about_vid' => 'index#about_vid'
+  get 'legal' => 'index#legal'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
